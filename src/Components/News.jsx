fca048f5e8118d3c90af66b1b79d26fe1139bde5
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-09-26 19:51:23 -0700

    Update news page with main section and side bar pic

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Image, Grid, Row, Col } from 'react-bootstrap';
 
 class News extends Component {
     render() {
         return (
             <div>
-                News
+                <Image src="assets/mountain-man.jpg" className="header-image" />
+                <Grid>
+                    <h2>News</h2>
+                    <Row>
+                        <Col xs={12} sm={8} className="main-section">
+                            <p>Great news</p>
+                        </Col>
+                        <Col xs={12} sm={4} className="sidebar-section">
+                            <Image src="assets/dog-people.jpg" />
+                            <p>Great news</p>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         );
     }
